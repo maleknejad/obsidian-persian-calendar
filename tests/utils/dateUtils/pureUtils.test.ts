438530bcac2dfd6cef63b912d1958cae5495a3a4
NedaMani <alireza3205@gmail.com>
2026-02-07 10:45:29 +0330

    fix: addDays test

--- tests/utils/dateUtils/pureUtils.test.ts
+++ tests/utils/dateUtils/pureUtils.test.ts
@@ -5,7 +5,7 @@ describe("addDays", () => {
 	it("should add days correctly", () => {
 		const base = new Date("2024-01-01");
 
-		const result = addDayDate(base, 1);
+		const result = addDayDate(base, 1).getDate();
 
 		expect(result).toBe(2);
 	});
@@ -13,7 +13,7 @@ describe("addDays", () => {
 	it("should not mutate original date", () => {
 		const base = new Date("2024-01-01");
 
-		const result = addDayDate(base, 5);
+		const result = addDayDate(base, 5).getDate();
 		expect(result).toBe(6);
 	});
 });
